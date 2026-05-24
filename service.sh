cc4edd603d63ba58847a99ccb51766ea9b349be2
Johnjk-mk <jejiro710@gmail.com>
2026-05-24 23:42:00 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-# Get the directory where the plugin is installed
+# Internal execution sequence
 MODDIR=${0%/*}
 
-# Run the modules using the relative path
+# 1. System & Thermal Management
 sh "$MODDIR/power_governor.sh"
 sh "$MODDIR/process_isolation.sh"
 sh "$MODDIR/gpu_pipeline.sh"
+
+# 2. Touch Response & Network (Critical for 4-finger claw/hip-fire tracking)
 sh "$MODDIR/input_latency.sh"
 sh "$MODDIR/network_stability.sh"
+
+# 3. Game State & Maintenance 
 sh "$MODDIR/fs_maintenance.sh"
 sh "$MODDIR/game_config.sh"
